27d5189f86943ea15edd142d3ce41041bc244aaa
Zakhar Ukhanov <ukhanovza@gmail.com>
2022-10-25 20:34:25 +0300

    zobr2: Fix adaptive

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -102,6 +102,8 @@ const MainPage = () => {
             <li>Понравилось</li>
             <li>Просмотренные</li>
           </ul>
+          <svg className={s.burger} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M21 18H3v-2h18v2Zm0-5H3v-2h18v2Zm0-5H3V6h18v2Z"/></svg>
+
         </div>
         </div>
     )
