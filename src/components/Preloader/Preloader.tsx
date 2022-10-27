2f44dd0effc78e8e5b21724ec2fac8d1ff5fb70a
Zakhar Ukhanov <ukhanovza@gmail.com>
2022-10-27 22:29:41 +0300

    zobr2: Add scroll bar and preloader

---
+++ src/components/Preloader/Preloader.tsx
@@ -0,0 +1,11 @@
+import s from './Preloader.module.scss'
+
+const Preloader = () => {
+    return(
+        <div className={s.Preloader}>
+            <h1>zobr</h1>
+        </div>
+    )
+}
+
+export default Preloader
