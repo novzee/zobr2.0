0c51ff841873b65ee6c0dcafdaa9be39497df58c
Zakhar Ukhanov <ukhanovza@gmail.com>
2022-12-01 23:50:59 +0300

    zobr2: Add info in filmPage and fix styles

--- src/pages/FilmPage/FilmPage.tsx
+++ src/pages/FilmPage/FilmPage.tsx
@@ -1,10 +1,31 @@
 import s from './FilmPage.module.scss'
 import { Link, useParams } from "react-router-dom";
 import roll from '../../assets/roll3.svg'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 
 const FilmPage = () => {
+    const [info, setInfo] = useState<any>({})
+    const [genres, setGenres] = useState<any>([])
     const {id} = useParams()
+    const url = `https://kinopoiskapiunofficial.tech/api/v2.2/films/${id}`
+    useEffect(()=>{
+        axios.get(url,
+            {headers: {
+                'X-API-KEY': '140dabd4-b4ac-4d65-b281-62e8d3b615c4',
+                'Content-Type': 'application/json',
+              }}
+              )
+              .then(function(responce) {
+                setInfo(responce.data)
+                console.log(responce.data)
+                console.log(info)
+                setGenres([...responce.data.genres])
+                console.log(genres)
+              }
+              )
+    }, [])
     return(
         <div className={s.FilmPage}>
             <div className={s.LeftSide}>
@@ -13,9 +34,15 @@ const FilmPage = () => {
                     <iframe src={`https://2554888813.svetacdn.in/N7TuPYwQNCQx?kp_id=${id}`} width="100%" height="100%" frameBorder="0" allowFullScreen></iframe>
                 </div>
                 <div className={s.info}>
-                    <h1>Name</h1>
-                    <p>2022</p>
+                    <h1>{info.nameRu}</h1>
+                    <p>{info.year}</p>
                 </div>
+                <div className={s.genres}>
+                    {genres.map((e:any) => {return(<h3 className={s.h3}>·{e.genre}</h3>)})}
+                </div>
+                <p className={s.description}>
+                    {info.description}
+                </p>
             </div>
             <div className={s.RightSide}>
                 <Link to='/movie'><h1>zobr</h1></Link>
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -92,7 +92,7 @@ const MainPage = () => {
                 <Link to={`/movie/${el.filmId || el.kinopoiskId}`}><div className={s.Item_img} style={{backgroundImage: `url(${el.posterUrlPreview})`}}><div className={s.back}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16"><path fill="currentColor" d="m11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/></svg>
                   </div></div></Link>
-                <p>{el.nameRu}</p>
+                <h5 className={s.name}>{el.nameRu}</h5>
               </div>
             ))}
           </div>
